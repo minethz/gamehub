9c7de2f2e5d2c05be665e3dc9f93b05939f905a0
Hiruni12345 <hirunidinoshi@gmail.com>
2024-06-15 22:37:51 +0530

    user profile new

--- app/src/main/java/com/example/noteapp/user_profile.java
+++ app/src/main/java/com/example/noteapp/user_profile.java
@@ -1,5 +1,52 @@
 package com.example.noteapp;
 
-public class user_profile {
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.dhaval2404.imagepicker.ImagePicker;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
+
+public class user_profile extends AppCompatActivity {
+    ImageView imageView ;
+    FloatingActionButton button;
+
+
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+
+        Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getColor(R.color.colorPrimaryDark)));
+
+        imageView= findViewById(R.id.imageView);
+        button=findViewById(R.id.floatingActionButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ImagePicker.with(user_profile.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri=data.getData();
+        imageView.setImageURI(uri);
+
+    }
 }
